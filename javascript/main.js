8b66ec3801e4631176b74446b444afa77fe0049d
Elingustafsson <elgusen@hotmail.com>
2018-01-31 13:09:36 +0100

    delite

--- javascript/main.js
+++ javascript/main.js
@@ -258,20 +258,6 @@ function runMap(MapCenter) {
   marker22.addListener('click', setModalTwentyTwo);
   marker23.addListener('click', setModalTwentyThree);
 
-  function setModal() {
-  var modal = document.getElementById('myModal');
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-  modal.style.display = "block";
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-}
 function setModalOne() {
     setModal();
     document.querySelector('.questions');
