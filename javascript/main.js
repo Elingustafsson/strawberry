2c5353256a5466114d49237de8196fcde7ce99a5
Korkken <lappalainentim@gmail.com>
2018-02-01 11:53:39 +0100

    changed markers to arrays

--- javascript/main.js
+++ javascript/main.js
@@ -26,223 +26,177 @@ function initMap(myPos) {
   myMap = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
   runMap(MapCenter);
 }
-
+  let presetDistance = 10;
 function runMap(MapCenter) {
   let beachmarker = null;
-  let newPoint = null;
+  let playerPos = null;
   function autoUpdate() {
     navigator.geolocation.getCurrentPosition(function(position) {
-      newPoint = new google.maps.LatLng(position.coords.latitude,
+      playerPos = new google.maps.LatLng(position.coords.latitude,
         position.coords.longitude);
 
       if (beachmarker) {
-        beachmarker.setPosition(newPoint);
+        beachmarker.setPosition(playerPos);
 
       } else {
         beachmarker = new google.maps.Marker({
-          position: newPoint,
+          position: playerPos,
           map: myMap,
           icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
         });
       }
-      myMap.setCenter(newPoint);
- var dist = google.maps.geometry.spherical.computeDistanceBetween(newPoint,origin);
- if (dist >= presetDistance)
-     {
-       marker.addListener('click', setModalOne);
-     }
- else if (dist > presetDistance)
-     {
-       google.maps.event.clearInstanceListeners(marker);
-     }
+      myMap.setCenter(playerPos);
+
+ var dist = google.maps.geometry.spherical.computeDistanceBetween(playerPos,markers[p]);
+ console.log(dist);
+       if (dist <= presetDistance)
+           {
+             markers[p].addListener('click', setModalOne);
+           }
+       else if (dist > presetDistance)
+           {
+             google.maps.event.clearInstanceListeners(markers);
+           }
 });
 
     }
+    var locations = [
+      [59.313670, 18.118675],
+      [59.312560, 18.105562],
+      [59.310822, 18.114690],
+      [59.312943, 18.109854],
+      [59.313680, 18.114184],
+      [59.311496, 18.118675],
+      [59.309242, 18.109821],
+      [59.309242, 18.104818],
+      [59.310603, 18.104818],
+      [59.313910, 18.123958],
+      [59.314142, 18.128604],
+      [59.314138, 18.132489],
+      [59.312397, 18.128854],
+      [59.312596, 18.132403],
+      [59.311934, 18.123680],
+      [59.310214, 18.119443],
+      [59.308769, 18.104712],
+      [59.310663, 18.109411],
+      [59.314359, 18.107662],
+      [59.314403, 18.109626],
+      [59.314836, 18.113413],
+      [59.315448, 19.118670],
+      [59.315706, 18.122586],
 
 
+    ];
 
   setInterval(autoUpdate, 1000);
-  presetDistance = 10;
 
-var origin = new google.maps.LatLng(59.312943, 18.109854);
-  var origin2 = new google.maps.LatLng(59.313680, 18.114184);
-  var origin3 = {
-    lat: 59.313670,
-    lng: 18.118675
-  };
-  var origin4 = {
-    lat: 59.312560,
-    lng: 18.105562
-  };
-  var origin5 = {
-    lat: 59.310822,
-    lng: 18.114690
-  };
-  var origin6 = {
-    lat: 59.311496,
-    lng: 18.118675
-  };
-  var origin7 = {
-    lat: 59.309242,
-    lng: 18.109821
-  };
-  var origin8 = {
-    lat: 59.309242,
-    lng: 18.114184
-  };
-  var origin9 = {
-    lat: 59.310603,
-    lng: 18.104818
-  };
-  var origin10 = {
-    lat: 59.313910,
-    lng: 18.123958
-  };
-  var origin11 = {
-    lat: 59.314142,
-    lng: 18.128604
-  };
-  var origin12 = {
-    lat: 59.314138,
-    lng: 18.132489
-  };
-  var origin13 = {
-    lat: 59.312397,
-    lng: 18.128854
-  };
-  var origin14 = {
-    lat: 59.312596,
-    lng: 18.132403
-  };
-  var origin15 = {
-    lat: 59.311934,
-    lng: 18.123680
-  };
-  var origin16 = {
-    lat: 59.310214,
-    lng: 18.119443
-  };
-  var origin17 = {
-    lat: 59.308769,
-    lng: 18.104712
-  };
-  var origin18 = {
-    lat: 59.310663,
-    lng: 18.109411
-  };
-  var origin19 = {
-    lat: 59.314359,
-    lng: 18.107662
-  };
-  var origin20 = {
-    lat: 59.314403,
-    lng: 18.109626
-  };
-  var origin21 = {
-    lat: 59.314836,
-    lng: 18.113413
-  };
-  var origin22 = {
-    lat: 59.315448,
-    lng: 18.118670
-  };
-  var origin23 = {
-    lat: 59.315706,
-    lng: 18.122586
-  };
+  let marker, p;
+  let markers = [];
+  for ( p = 0; p < locations.length; p++)
+  {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[p][0], locations[p][1]),
+      map: myMap
 
-  var marker = new google.maps.Marker({
-    position: origin,
-    map: myMap
-  });
-  var marker2 = new google.maps.Marker({
-    position: origin2,
-    map: myMap
-  });
-  var marker3 = new google.maps.Marker({
-    position: origin3,
-    map: myMap
-  });
-  var marker4 = new google.maps.Marker({
-    position: origin4,
-    map: myMap
-  });
-  var marker5 = new google.maps.Marker({
-    position: origin5,
-    map: myMap
-  });
-  var marker6 = new google.maps.Marker({
-    position: origin6,
-    map: myMap
-  });
-  var marker7 = new google.maps.Marker({
-    position: origin7,
-    map: myMap
-  });
-  var marker8 = new google.maps.Marker({
-    position: origin8,
-    map: myMap
-  });
-  var marker9 = new google.maps.Marker({
-    position: origin9,
-    map: myMap
-  });
-  var marker10 = new google.maps.Marker({
-    position: origin10,
-    map: myMap
-  });
-  var marker11 = new google.maps.Marker({
-    position: origin11,
-    map: myMap
-  });
-  var marker12 = new google.maps.Marker({
-    position: origin12,
-    map: myMap
-  });
-  var marker13 = new google.maps.Marker({
-    position: origin13,
-    map: myMap
-  });
-  var marker14 = new google.maps.Marker({
-    position: origin14,
-    map: myMap
-  });
-  var marker15 = new google.maps.Marker({
-    position: origin15,
-    map: myMap
-  });
-  var marker16 = new google.maps.Marker({
-    position: origin16,
-    map: myMap
-  });
-  var marker17 = new google.maps.Marker({
-    position: origin17,
-    map: myMap
-  });
-  var marker18 = new google.maps.Marker({
-    position: origin18,
-    map: myMap
-  });
-  var marker19 = new google.maps.Marker({
-    position: origin19,
-    map: myMap
-  });
-  var marker20 = new google.maps.Marker({
-    position: origin20,
-    map: myMap
-  });
-  var marker21 = new google.maps.Marker({
-    position: origin21,
-    map: myMap
-  });
-  var marker22 = new google.maps.Marker({
-    position: origin22,
-    map: myMap
-  });
-  var marker23 = new google.maps.Marker({
-    position: origin23,
-    map: myMap
-  });
+    });
+    markers.push(marker);
+    markers[p].addListener('click', function(){
+      setModal();
+    });
+
+  }
+  // var marker = new google.maps.Marker({
+  //   position: origin,
+  //   map: myMap
+  // });
+  // var marker2 = new google.maps.Marker({
+  //   position: origin2,
+  //   map: myMap
+  // });
+  // var marker3 = new google.maps.Marker({
+  //   position: origin3,
+  //   map: myMap
+  // });
+  // var marker4 = new google.maps.Marker({
+  //   position: origin4,
+  //   map: myMap
+  // });
+  // var marker5 = new google.maps.Marker({
+  //   position: origin5,
+  //   map: myMap
+  // });
+  // var marker6 = new google.maps.Marker({
+  //   position: origin6,
+  //   map: myMap
+  // });
+  // var marker7 = new google.maps.Marker({
+  //   position: origin7,
+  //   map: myMap
+  // });
+  // var marker8 = new google.maps.Marker({
+  //   position: origin8,
+  //   map: myMap
+  // });
+  // var marker9 = new google.maps.Marker({
+  //   position: origin9,
+  //   map: myMap
+  // });
+  // var marker10 = new google.maps.Marker({
+  //   position: origin10,
+  //   map: myMap
+  // });
+  // var marker11 = new google.maps.Marker({
+  //   position: origin11,
+  //   map: myMap
+  // });
+  // var marker12 = new google.maps.Marker({
+  //   position: origin12,
+  //   map: myMap
+  // });
+  // var marker13 = new google.maps.Marker({
+  //   position: origin13,
+  //   map: myMap
+  // });
+  // var marker14 = new google.maps.Marker({
+  //   position: origin14,
+  //   map: myMap
+  // });
+  // var marker15 = new google.maps.Marker({
+  //   position: origin15,
+  //   map: myMap
+  // });
+  // var marker16 = new google.maps.Marker({
+  //   position: origin16,
+  //   map: myMap
+  // });
+  // var marker17 = new google.maps.Marker({
+  //   position: origin17,
+  //   map: myMap
+  // });
+  // var marker18 = new google.maps.Marker({
+  //   position: origin18,
+  //   map: myMap
+  // });
+  // var marker19 = new google.maps.Marker({
+  //   position: origin19,
+  //   map: myMap
+  // });
+  // var marker20 = new google.maps.Marker({
+  //   position: origin20,
+  //   map: myMap
+  // });
+  // var marker21 = new google.maps.Marker({
+  //   position: origin21,
+  //   map: myMap
+  // });
+  // var marker22 = new google.maps.Marker({
+  //   position: origin22,
+  //   map: myMap
+  // });
+  // var marker23 = new google.maps.Marker({
+  //   position: origin23,
+  //   map: myMap
+  // });
 
   for (var i = 0; i < lines.length; i++) {  myMap.data.add({
       geometry: new google.maps.Data.Polygon([
@@ -250,144 +204,157 @@ var origin = new google.maps.LatLng(59.312943, 18.109854);
       ])
     })
   }
-  marker.addListener('click', setModalOne);
-  marker2.addListener('click', setModalTwo);
-  marker3.addListener('click', setModalThree);
-  marker4.addListener('click', setModalFour);
-  marker5.addListener('click', setModalFive);
-  marker6.addListener('click', setModalSix);
-  marker7.addListener('click', setModalSeven);
-  marker8.addListener('click', setModalEight);
-  marker9.addListener('click', setModalNine);
-  marker10.addListener('click', setModalTen);
-  marker11.addListener('click', setModalEleven);
-  marker12.addListener('click', setModalTwelve);
-  marker13.addListener('click', setModalThirteen);
-  marker14.addListener('click', setModalFourteen);
-  marker15.addListener('click', setModalFifteen);
-  marker16.addListener('click', setModalSixteen);
-  marker17.addListener('click', setModalSeventeen);
-  marker18.addListener('click', setModalEightteen);
-  marker19.addListener('click', setModalNineteen);
-  marker20.addListener('click', setModalTwenty);
-  marker21.addListener('click', setModalTwentyOne);
-  marker22.addListener('click', setModalTwentyTwo);
-  marker23.addListener('click', setModalTwentyThree);
+  // marker.addListener('click', setModalOne);
+  // marker2.addListener('click', setModalTwo);
+  // marker3.addListener('click', setModalThree);
+  // marker4.addListener('click', setModalFour);
+  // marker5.addListener('click', setModalFive);
+  // marker6.addListener('click', setModalSix);
+  // marker7.addListener('click', setModalSeven);
+  // marker8.addListener('click', setModalEight);
+  // marker9.addListener('click', setModalNine);
+  // marker10.addListener('click', setModalTen);
+  // marker11.addListener('click', setModalEleven);
+  // marker12.addListener('click', setModalTwelve);
+  // marker13.addListener('click', setModalThirteen);
+  // marker14.addListener('click', setModalFourteen);
+  // marker15.addListener('click', setModalFifteen);
+  // marker16.addListener('click', setModalSixteen);
+  // marker17.addListener('click', setModalSeventeen);
+  // marker18.addListener('click', setModalEightteen);
+  // marker19.addListener('click', setModalNineteen);
+  // marker20.addListener('click', setModalTwenty);
+  // marker21.addListener('click', setModalTwentyOne);
+  // marker22.addListener('click', setModalTwentyTwo);
+  // marker23.addListener('click', setModalTwentyThree);
+
+
+  // let arrayFunctions = [
+  //   function setModalOne() {
+  //       setModal();
+  //       document.querySelector('.questions');
+  //     },
+  //     function setModalTwo() {
+  //       setModal();
+  //       document.querySelector('.questions');
+  //     }
+  //
+  // ]
 
 function setModalOne() {
     setModal();
     document.querySelector('.questions');
   }
-
-  function setModalTwo() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalThree() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalFour() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalFive() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalSix() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalSeven() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalEight() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalNine() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalEleven() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTwelve() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalThirteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalFourteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalFifteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalSixteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalSeventeen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalEightteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalNineteen() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTwenty() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTwentyOne() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTwentyTwo() {
-    setModal();
-    document.querySelector('.questions');
-  }
-
-  function setModalTwentyThree() {
-    setModal();
-    document.querySelector('.questions');
-  }
+  //
+  // function setModalTwo() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalThree() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalFour() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalFive() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalSix() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalSeven() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalEight() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalNine() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalEleven() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTwelve() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalThirteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalFourteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalFifteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalSixteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalSeventeen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalEightteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalNineteen() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTwenty() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTwentyOne() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTwentyTwo() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
+  //
+  // function setModalTwentyThree() {
+  //   setModal();
+  //   document.querySelector('.questions');
+  // }
 }
 let lines =[
    line1 = [{
