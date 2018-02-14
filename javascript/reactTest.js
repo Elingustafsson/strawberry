b2b242959a28152bcb0aadc859f0c9d3e8970f7d
Elingustafsson <elgusen@hotmail.com>
2018-02-14 11:39:51 +0100

    add some new content to profile

--- javascript/reactTest.js
+++ javascript/reactTest.js
@@ -2,8 +2,8 @@ const e = React.createElement;
 
 ReactDOM.render(
     e('a', { href: "https://google.se" },
-      e('img', { src: "../img/fact.png"})
+      e('img', { src: "../img/profile.jpg"})
 ),
-  document.getElementById('react-pic')
+  document.getElementById('reactPic')
 
 );
