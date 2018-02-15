3927be04182fa25c5924a729df653d285e6f9562
Elingustafsson <elgusen@hotmail.com>
2018-02-15 10:25:36 +0100

    add react element to page

--- javascript/reactTest.js
+++ javascript/reactTest.js
@@ -1,9 +1,6 @@
-const e = React.createElement;
+const e = React.createElement
 
 ReactDOM.render(
-    e('a', { href: "https://google.se" },
-      e('img', { src: "../img/profile.jpg"})
-),
-  document.getElementById('reactPic')
-
-);
+  e('a', { href: "https://google.se" },
+    e('img', { src: "../img/profile.jpg", width: '100px', height: '100px'})
+), document.getElementById('reactPic'))
