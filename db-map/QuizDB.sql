57c972aec6844285f586d3bbf51abb7429cab1e5
V-Stenq <32197982+V-Stenq@users.noreply.github.com>
2018-02-19 11:03:23 +0100

    exported db

--- db-map/QuizDB.sql
+++ db-map/QuizDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `questions` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `questions`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: questions
@@ -105,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-19 10:59:48
+-- Dump completed on 2018-02-19 11:03:02
