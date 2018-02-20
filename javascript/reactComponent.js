d93f72dfa2f33baf353ba20fbdca1524f29f0581
Korkken <lappalainentim@gmail.com>
2018-02-20 12:12:46 +0100

    moved files to old stuff

--- javascript/reactComponent.js
+++ javascript/reactComponent.js
@@ -1,6 +1,6 @@
 const e = React.createElement
 
 ReactDOM.render(
-  e('a', { href: "https://google.se" },
+  e('a', { href: "http://i0.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg" },
     e('img', { src: "../img/profile.jpg", width: '100px', height: '100px'})
 ), document.getElementById('reactPic'))
